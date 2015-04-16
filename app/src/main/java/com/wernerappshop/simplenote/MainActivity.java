dd67c133c73de6bacbba6ff6b7838094f9af95ba
Michael Werner <michaelw3@gmail.com>
2015-04-16 11:59:13 +0300

    trying to fix git conflicts

--- app/src/main/java/com/wernerappshop/simplenote/MainActivity.java
+++ app/src/main/java/com/wernerappshop/simplenote/MainActivity.java
@@ -1,34 +1,52 @@
 package com.wernerappshop.simplenote;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mDrawerItems;
+    private Toolbar mToolbar;
+    private CharSequence mTitle;
+    private LinearLayout draweLinerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_drawer);
         // Set the ActionBar
-
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        draweLinerLayout = (LinearLayout)findViewById(R.id.draweLinerLayout);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        mDrawerItems = getResources().getStringArray(R.array.drawer_list);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
         mDrawerToggle= new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar, R.string.app_name, R.string.app_name);
+        mDrawerList = (ListView) findViewById(R.id.drawerLV);
+
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mDrawerToggle.syncState();
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, mDrawerItems));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
         // Set an OnMenuItemClickListener to handle menu item clicks
         mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
@@ -62,5 +80,40 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Log.i("Drawer","item clicked");
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new NoteFragment();
+        Bundle args = new Bundle();
+        args.putInt("ARG_PARAM1", position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mToolbar.setTitle(mDrawerItems[position]);
+        mDrawerLayout.closeDrawer(draweLinerLayout);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        mToolbar.setTitle(mTitle);
     }
 }
